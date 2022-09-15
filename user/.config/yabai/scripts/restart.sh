0311a00fb108cfb739c9237ed54c34d4b972a93b
Bohdan Yatsiuk <emailbogdans@gmail.com>
2022-09-15 10:49:09 +0300

    restart yabai using launchctl

--- user/.config/yabai/scripts/restart.sh
+++ user/.config/yabai/scripts/restart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-~/dotfiles/scripts/runservice.sh yabai
-~/dotfiles/scripts/runservice.sh sketchybar
+# ~/dotfiles/scripts/runservice.sh yabai
+# ~/dotfiles/scripts/runservice.sh sketchybar
+launchctl stop homebrew.mxcl.yabai
+launchctl start homebrew.mxcl.yabai
 
 osascript -e 'display notification "yabai restarted" with title "Karabiner"'
